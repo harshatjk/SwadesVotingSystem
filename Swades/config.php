6eb0a528fc466f6d5140772ed3390741437916f8
adityadev91 <adityadev91@rediffmail.com>
2015-10-03 11:39:07 -0700

    Update config.php

--- Swades/config.php
+++ Swades/config.php
@@ -1,7 +1,7 @@
 <?php
 // Database Parameters
-define("SERVER","dagobah.engr.scu.edu");
-define("USERNAME","rdokania");
-define("PASSWORD","2015Swades-16");
-define("DBNAME","sdb_rdokania");
+define("SERVER","");
+define("USERNAME","");
+define("PASSWORD","");
+define("DBNAME","");
 ?>
